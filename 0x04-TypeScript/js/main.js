f11cc0c7e88e1b03ab8f7f400566e1280c1fdaec
emmanuelofor <emmanuel.ofori@kibo.school>
2023-10-04 12:35:18 +0000

    Task 0

---
+++ 0x04-TypeScript/js/main.js
@@ -0,0 +1,32 @@
+var student1 = {
+    firstName: "Kwaku",
+    lastName: "Agyeman",
+    age: 30,
+    location: "Kumasi"
+};
+var student2 = {
+    firstName: "Ola",
+    lastName: "Michaels",
+    age: 39,
+    location: "Lagos"
+};
+var studentsList = [student1, student2];
+var table = document.createElement('table');
+var tbody = document.createElement('tbody');
+table.style.background = "pink";
+table.appendChild(tbody);
+studentsList.forEach(function (student) {
+    var row = document.createElement('tr');
+    var nameCell = document.createElement('td');
+    var locationCell = document.createElement('td');
+    nameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+    nameCell.style.border = "1px solid pink";
+    locationCell.style.border = "1px solid pink";
+    nameCell.style.padding = "5px";
+    locationCell.style.padding = "5px";
+    row.appendChild(nameCell);
+    row.appendChild(locationCell);
+    tbody.appendChild(row);
+});
+document.body.appendChild(table);
